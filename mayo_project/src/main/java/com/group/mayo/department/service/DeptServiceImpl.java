79d23cfbae53f9545604a78fcb9dc879351aa0ae
RealFantastic <wjdghks5698@gmail.com>
2022-06-14 00:05:13 +0900

    TransactionManager 등록(@Transactional)

--- mayo_project/src/main/java/com/group/mayo/department/service/DeptServiceImpl.java
+++ mayo_project/src/main/java/com/group/mayo/department/service/DeptServiceImpl.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.group.mayo.department.dao.DeptDao;
 import com.group.mayo.department.domain.Dept;
 
 @Service
+@Transactional
 public class DeptServiceImpl implements DeptService {
 	
 	@Autowired
